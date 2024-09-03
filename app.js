dc5312a3efc34671b79d59091ba36a17df02f7ea
Bakhriddinow <sardorbakhriddinow@gmail.com>
2024-09-03 11:21:42 +0500

    i add contact part

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const NavMenuBtn = document.getElementById("menu");
-const NavLink = document.getElementById('nav-res')
+const NavLink = document.getElementById('nav-res');
+
 NavMenuBtn.addEventListener("click", () => {
-    if(NavLink.style.left === '-100%'){
-        NavLink.style.left = '0'
-    } else{
-        NavLink.style.left = '-100%'
-    }
+    NavLink.classList.toggle('active');
 });
